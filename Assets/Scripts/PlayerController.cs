9738fffec72945b87df17a83ddfeacae9e9a2e22
space-zeddie <thespacezeddie@gmail.com>
2016-04-14 20:18:43 +0300

    added basic network funciton

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour 
+{
+    /* only for learning networking */
+    void Update()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(x, y, 0);
+    }
+}
